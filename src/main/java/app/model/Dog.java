db7e5196189aad9405ec274f42199f04732046ac
Vladimir Solovyov <svn131131@gmail.com>
2023-04-02 18:34:40 +0500

    Gotovo 02-04-2023

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+
 public class Dog extends Animal {
     @Override
     public String toString() {
